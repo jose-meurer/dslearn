f73f539b1cf134b916a65d9f6401bc8faeaf5c0b
JoseMeurer <josewm18@gmail.com>
2022-09-14 11:16:50 -0300

    FEAT Repositories

---
+++ backend/src/main/java/com/josemeurer/dslearn/repositories/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.josemeurer.dslearn.repositories;
+
+import com.josemeurer.dslearn.entities.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
---
+++ backend/src/main/java/com/josemeurer/dslearn/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.josemeurer.dslearn.repositories;
+
+import com.josemeurer.dslearn.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
